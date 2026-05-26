6f615d04d41266b3a6d3865f6415a9c52a780f65
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-26 09:32:47 +0200

    Tableau pour les phrases

--- jour06/job02/script.js
+++ jour06/job02/script.js
@@ -10,5 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 
+//------ Bouton Blade Runner ---    
+    const citations = [
+        "J'ai vu tant de choses que vous, humains, ne pourriez pas croire...",
+        "Tous ces moments se perdront dans l'oubli comme des larmes dans la pluie.",
+        "Réveillez-vous ! Il est temps de mourir.",
+        "La lumière qui brille deux fois plus fort brûle deux fois moins longtemps.",
+        "C'est dommage qu'elle doive mourir. Mais on en est tous là, non ?"
+    ];
 
+    
 });
